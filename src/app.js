3b946bc512ccb853cf4f01e5f43d4706dc5d876d
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 23:57:37 +0530

    used validators

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.patch("/user/:userId", async (req, res) => {
     if (!isUpdateAllowed) {
       throw new Error("Update not allowed");
     }
-    if (data?.skills.length > 10) {
+    if (data?.skills?.length > 10) {
       throw new Error("Skills cannot be more than 10");
     }
     await User.findByIdAndUpdate(userId, data, {
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -6,12 +7,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      maxlength:50
+      maxlength: 50,
     },
     lastName: {
-        type: String,
-        trim: true,
-        maxlength:50
+      type: String,
+      trim: true,
+      maxlength: 50,
     },
     emailId: {
       type: String,
@@ -19,10 +20,20 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email " + value);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password " + value);
+        }
+      },
     },
     age: {
       type: Number,
@@ -39,6 +50,11 @@ const userSchema = new mongoose.Schema(
     photoUrl: {
       type: String,
       default: "https://bhuvanaconsultancy.in/images/Men-Profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL " + value);
+        }
+      },
     },
     about: {
       type: String,
