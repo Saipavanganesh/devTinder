fa3fd79ad03bb6619c4eb7a11b369a490fba1c5e
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 10:15:29 +0530

    Route handling examples

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const app = express();
+
+//read from url => /user?userId=101&name="ganesh"
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
+});
+
+
+//read from url =>/user/101/ganesh/testPassword
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
+  });
+
+
+
+
+
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
 });
 app.use("/hello", (req, res) => {
   res.send("Hello page");
+});
+app.use("/testing", (req, res) => {
+  res.send("This is the testing page");
 });
 app.use("/", (req, res) => {
   res.send("This is the landing page");
