ae24d5b2471786e94873d919beb1a3bf4f2bf7b2
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 10:50:38 +0530

    Multiple Routehandlers and middlewares

--- src/app.js
+++ src/app.js
@@ -2,34 +2,39 @@ const express = require("express");
 const app = express();
 
 //read from url => /user?userId=101&name="ganesh"
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
-});
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
+// });
+// //read from url =>/user/101/ganesh/testPassword
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//     console.log(req.params);
+//     res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
+//   });
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the server");
+// });
+// app.use("/hello", (req, res) => {
+//   res.send("Hello page");
+// });
+// app.use("/testing", (req, res) => {
+//   res.send("This is the testing page");
+// });
 
+//Multiple route handlers because these are middlewares.
+// Function which actually sends the response is called request handler, all the remaining functions are called Middlewares
+//GET /user => chain of middlewares => request handler
+app.use(
+  "/user",
+  (req, res, next) => {
+    next();
+    // res.send("1st Response");
+  },
+  (req, res) => {
+    res.send("2nd Response");
+  }
+);
 
-//read from url =>/user/101/ganesh/testPassword
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
-  });
-
-
-
-
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
-app.use("/hello", (req, res) => {
-  res.send("Hello page");
-});
-app.use("/testing", (req, res) => {
-  res.send("This is the testing page");
-});
-app.use("/", (req, res) => {
-  res.send("This is the landing page");
-});
 app.listen(7777, () => {
   console.log("Listening on port 3k");
 });
