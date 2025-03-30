e9b41d63ddcf447d58d3f0a9aabb5a4840b808fb
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 19:03:17 +0530

    written get apis

--- src/app.js
+++ src/app.js
@@ -6,12 +6,54 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("User added succesfully");
   } catch (err) {
     res.status(400).send(`Error in saving user: ${err.message}`);
+  }
+});
+
+//GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.status(500).send("Something went wrong");
+  }
+});
+//GET one user by email
+app.get("/oneUser", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    if (users !== 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch(err) {
+    res.status(500).send(`Something went wrong ${err.message}`);
+  }
+});
+
+//GET all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("Users not found");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.status(500).send("Something went wrong");
   }
 });
 
