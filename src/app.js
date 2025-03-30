9fe2ca8118675813329405ae7e91d855e1351979
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 11:25:07 +0530

    Wildcard error handling

--- src/app.js
+++ src/app.js
@@ -1,24 +1,17 @@
 const express = require("express");
 const app = express();
-const { adminAuth, userAuth } = require("./middlewares/auth");
-//Handle Auth middleware requsest for all methods for "/admin" routes
-app.use("/admin", adminAuth);
 
-//Response handler
-app.get("/admin/", (req, res) => {
-  res.send("All Admin Data Sent");
-});
-//Response handler
-app.delete("/admin/", (req, res) => {
-  res.send("All Admin Data Deleted");
+app.get("/user/", (req, res) => {
+  throw new Error("ldfjkjflkdfljad");
+  res.send("All User Data Sent...");
 });
 
-app.post("/user/", (req, res) => {
-  res.send("User logged in Succesfully");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
-app.get("/user/", userAuth, (req, res) => {
-  res.send("All User Data Sent");
-});
+
 app.listen(7777, () => {
   console.log("Listening on port 3k");
 });
