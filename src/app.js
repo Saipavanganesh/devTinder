8e4b98f93f8ba1fd3a918b4089098b624ec7e29e
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 11:17:17 +0530

    Middlewares for authorization

--- src/app.js
+++ src/app.js
@@ -1,40 +1,24 @@
 const express = require("express");
 const app = express();
+const { adminAuth, userAuth } = require("./middlewares/auth");
+//Handle Auth middleware requsest for all methods for "/admin" routes
+app.use("/admin", adminAuth);
 
-//read from url => /user?userId=101&name="ganesh"
-// app.get("/user", (req, res) => {
-//   console.log(req.query);
-//   res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
-// });
-// //read from url =>/user/101/ganesh/testPassword
-// app.get("/user/:userId/:name/:password", (req, res) => {
-//     console.log(req.params);
-//     res.send({ firstname: "Sai Pavan Ganesh", lastname: "Mallampalli" });
-//   });
-// app.use("/test", (req, res) => {
-//   res.send("Hello from the server");
-// });
-// app.use("/hello", (req, res) => {
-//   res.send("Hello page");
-// });
-// app.use("/testing", (req, res) => {
-//   res.send("This is the testing page");
-// });
+//Response handler
+app.get("/admin/", (req, res) => {
+  res.send("All Admin Data Sent");
+});
+//Response handler
+app.delete("/admin/", (req, res) => {
+  res.send("All Admin Data Deleted");
+});
 
-//Multiple route handlers because these are middlewares.
-// Function which actually sends the response is called request handler, all the remaining functions are called Middlewares
-//GET /user => chain of middlewares => request handler
-app.use(
-  "/user",
-  (req, res, next) => {
-    next();
-    // res.send("1st Response");
-  },
-  (req, res) => {
-    res.send("2nd Response");
-  }
-);
-
+app.post("/user/", (req, res) => {
+  res.send("User logged in Succesfully");
+});
+app.get("/user/", userAuth, (req, res) => {
+  res.send("All User Data Sent");
+});
 app.listen(7777, () => {
   console.log("Listening on port 3k");
 });
