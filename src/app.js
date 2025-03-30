3ae76dfe1b43d309ab0b3101b3b61e247c8089b6
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-03-30 17:29:29 +0530

    Adding dynamicuser from reqeust

--- src/app.js
+++ src/app.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const User = require("./models/user");
-
 const app = express();
+const User = require("./models/user");
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Kartikeya Murali Krishna",
-    lastName: "Mallampalli",
-    emailId: "kmk@gmail.com",
-    password: "Kmk@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added succesfully");
   } catch (err) {
-    res.status(400).send("Error in saving user: ", err.message);
+    res.status(400).send(`Error in saving user: ${err.message}`);
   }
 });
 
 connectDB()
   .then(() => {
     console.log("Database connection established");
-
     app.listen(7777, () => {
       console.log("Listening on port 7777");
     });
