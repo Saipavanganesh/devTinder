0b6ea1edfe99076ea4671a9c00274a044b70e80b
locobuzz-ganesh <saipavan.ganesh@locobuzz.com>
2025-04-09 23:27:04 +0530

    created api to check our connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const User = require("../models/user");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 userRouter.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
@@ -60,14 +62,31 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate(
-      "fromUserId",
-      "firstName lastName photoUrl age gender about skills"
-    );
+    }).populate("fromUserId", USER_SAFE_DATA);
     // }).populate("fromUserId", ["firstName", "lastName","photoUrl"]);
     res.json({
       message: "Connection requests fetched",
       data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send(`Error: ${err.message}`);
+  }
+});
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((request) => request.fromUserId);
+
+    res.json({
+      message: "Connection requests fetched",
+      data: data,
     });
   } catch (err) {
     res.status(400).send(`Error: ${err.message}`);
